89331f8ba5af3603ac4e697ffd540ffc9ad68720
nxnarbais <nicolas.narbais@datadoghq.com>
2019-10-24 10:49:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+git clone git@github.com:benc-uk/nodejs-demoapp.git app
+cd app/
+npm install
+npm start
+
+see if it works
---
+++ mongo-init.js
@@ -0,0 +1,31 @@
+// Create user to read and write records
+db.createUser(
+    {
+        user: "dduser",
+        pwd: "dtdgpwd123",
+        roles: [
+            {
+                role: "readWrite",
+                db: "dtdg" // Look for env variable: MONGO_INITDB_DATABASE
+            }
+        ]
+    }
+);
+
+// Insert a few records
+db.dogs.insert({ name: "bits1" });
+
+// Create an admin user
+// FIXME: At the moment, this create an admin user in the dtdg database. To make it work, it should be created on the admin database.
+// use admin;
+db.createUser(
+    {
+        user: "datadog",
+        pwd: "ddpwd123",
+        roles : [
+            {role: 'read', db: 'admin' },
+            {role: 'clusterMonitor', db: 'admin'},
+            {role: 'read', db: 'local' }
+        ]
+    }
+);
